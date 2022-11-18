367fa2591973599cb96d72f74d4d37d4b06ed7d8
Ror-Dev-Ang <ror.dev.ang@gmail.com>
2022-11-18 17:02:26 -0500

    Finished the views but missign the connection to back

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Work.scss';
 
@@ -130,4 +130,8 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work')
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg'
+);
